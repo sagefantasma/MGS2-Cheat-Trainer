fcf0706140e268f61376a788ef65a554b7368ba6
Andrew Naro <a.naro1995@gmail.com>
2023-12-22 23:23:11 -0500

    started to work on logic for adding a controller hook to the trainerso you can modify values without tabbing out

---
+++ MGS2-MC/ControllerHook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SharpDX.XInput;
+
+
+namespace MGS2_MC
+{
+
+    internal class ControllerHook
+    {
+        //Controller[] controllerList = new[] {new Controller(UserIndex.One), ...}
+        Controller controller = new Controller(UserIndex.Any);
+        public event EventHandler TrainerMenu;
+
+        internal void Start(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                State previousControllerState = controller.GetState();
+                while (controller.IsConnected)
+                {
+                    State controllerState = controller.GetState();
+                    if(previousControllerState.PacketNumber != controllerState.PacketNumber)
+                    {
+                        if (IsMenuRequestCombination(controllerState))
+                        {
+                            OnTrainerMenu(EventArgs.Empty); //trigger the event for the injector to leverage
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsMenuRequestCombination(State controllerState)
+        {
+            if(controllerState.Gamepad.Buttons == GamepadButtonFlags.Start && 
+                controllerState.Gamepad.Buttons == GamepadButtonFlags.Back)
+            {
+                //this is just a placeholder case while I figure out how to do it for realsies. no way this works as is
+                return true;
+            }
+
+            return false;
+        }       
+
+        protected virtual void OnTrainerMenu(EventArgs e)
+        {
+            TrainerMenu?.Invoke(this, e);
+        }
+    }
+}
---
+++ MGS2-MC/MGS2Injector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGS2_MC
+{
+    internal static class MGS2Injector
+    {
+        static ControllerHook ControllerHook = new ControllerHook();
+
+        internal static async Task EnableInjector()
+        {
+            await Task.Run(StartControllerHook);
+        }
+
+        private static void StartControllerHook()
+        {
+            ControllerHook.TrainerMenu += OpenTrainerMenu;
+            ControllerHook.Start(System.Threading.CancellationToken.None); //TODO: maybe do a proper cancellation token in the future?
+        }
+
+        private static void OpenTrainerMenu(object o, EventArgs e)
+        {
+            //TODO: open a GUI over MGS2 that lets the user do their desired modifications
+        }
+    }
+}

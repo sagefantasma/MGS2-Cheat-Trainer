23cafc954df78342bbeac63bfb691b419b603d40
Andrew Naro <a.naro1995@gmail.com>
2024-01-11 23:06:41 -0500

    fixed code duplication issue in TrainerConfigStructure. thank you build action step!!

--- MGS2-MC/TrainerConfigStructure.cs
+++ MGS2-MC/TrainerConfigStructure.cs
@@ -57,32 +57,5 @@ namespace MGS2_MC
                 return false;
             }
         }
-
-        public static bool BuildTrainerConfigFile(string fileLocation, TrainerConfig baseConfig = null)
-        {
-            try
-            {
-                using (StreamWriter writer = new StreamWriter(fileLocation))
-                {
-                    if (baseConfig == null)
-                    {
-                        baseConfig = new TrainerConfig
-                        {
-                            AutoLaunchGame = true,
-                            CloseGameWithTrainer = false,
-                            CloseTrainerWithGame = true,
-                            Mgs2ExePath = "C:/Program Files (x86)/Steam/steamapps/common/MGS2/METAL GEAR SOLID2.exe"
-                        };
-                    }
-                    writer.WriteLine(JsonSerializer.Serialize(baseConfig));
-
-                    return true;
-                }
-            }
-            catch
-            {
-                return false;
-            }
-        }
     }
 }

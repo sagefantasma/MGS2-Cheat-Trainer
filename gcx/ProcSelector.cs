f8b1a8a670ad1ecdef77f27f92270563c5ee96ea
Andrew Naro <a.naro1995@gmail.com>
2024-11-24 00:40:18 -0500

    added the rest of the known procs we need for base game. added logic to automatically add proc dependencies when they are selected to be added.

--- gcx/ProcSelector.cs
+++ gcx/ProcSelector.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace gcx
 {
@@ -24,6 +25,24 @@ namespace gcx
         {
             procListBox.DataSource = KnownProc.SpawnProcs;
             procListBox.DisplayMember = "CommonName";
+            InitializeProcDependencies();
+        }
+
+        private void InitializeProcDependencies()
+        {
+            KnownProc.GunCheck.ProcDependencies = new RawProc[] { KnownProc.AwardM9Ammo, KnownProc.AwardUspAmmo,
+                KnownProc.AwardSocomAmmo, KnownProc.AwardM4Ammo, KnownProc.AwardPsg1Ammo, KnownProc.AwardPsg1tAmmo,
+                KnownProc.AwardRgbAmmo, KnownProc.AwardNikitaAmmo, KnownProc.AwardStingerAmmo, KnownProc.AwardAksAmmo,
+                KnownProc.AwardC4, KnownProc.AwardChaffG, KnownProc.AwardStunG, KnownProc.AwardGrenade,
+                KnownProc.AwardBook, KnownProc.AwardRation, KnownProc.AwardBandages, KnownProc.AwardPentazemin };
+        }
+
+        private DecodedProc ConvertRawProcToDecodedProc(RawProc rawProc)
+        {
+            FileInfo procFile = new FileInfo($"MGS2 Known Procs/proc_0x{rawProc.BigEndianRepresentation}.proc");
+            string procName = procFile.Name.Replace("proc_0x", "").Replace(".proc", "");
+            uint order = Convert.ToUInt32(procName.Trim(), 16);
+            return new DecodedProc(procName, order, File.ReadAllBytes(procFile.FullName), null, 0, 0);
         }
 
         private void addProcsButton_Click(object sender, EventArgs e)
@@ -31,14 +50,26 @@ namespace gcx
             foreach(var proc in procListBox.CheckedItems)
             {
                 RawProc rawProc = proc as RawProc;
-                FileInfo procFile = new FileInfo($"MGS2 Known Procs/proc_0x{rawProc.BigEndianRepresentation}.proc");
-                string procName = procFile.Name.Replace("proc_0x", "").Replace(".proc", "");
-                uint order = Convert.ToUInt32(procName.Trim(), 16);
-                DecodedProc procedure = new DecodedProc(procName, order, File.ReadAllBytes(procFile.FullName), null, 0, 0);
+                DecodedProc procedure = ConvertRawProcToDecodedProc(rawProc);
                 ProcsToAdd.Add(procedure);
+                AddDependencies(rawProc);
             }
+            //MessageBox.Show($"Adding {ProcsToAdd.Count} procs!");
             this.DialogResult = DialogResult.OK;
             Close();
+        }
+
+        private void AddDependencies(RawProc proc)
+        {
+            if (proc.ProcDependencies != null)
+            {
+                foreach (RawProc dependency in proc.ProcDependencies)
+                {
+                    AddDependencies(dependency);
+                    if(!ProcsToAdd.Any(alreadyQueuedProc => alreadyQueuedProc.Name == dependency.BigEndianRepresentation))
+                        ProcsToAdd.Add(ConvertRawProcToDecodedProc(dependency));
+                }
+            }
         }
 
         private void cancelButton_Click(object sender, EventArgs e)

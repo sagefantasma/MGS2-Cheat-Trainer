2190fcb0ddf58646e17b6f0a67466522e7f95aa5
Andy <sagefantasma@gmail.com>
2026-07-10 21:40:20 -0400

    The incremental progress increments.

---
+++ MGS2_CheatTrainer_V2/Views/StringDetailView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MGS2_CheatTrainer_V2.Views;
+
+public partial class StringDetailView : UserControl
+{
+    public StringDetailView()
+    {
+        InitializeComponent();
+    }
+}
